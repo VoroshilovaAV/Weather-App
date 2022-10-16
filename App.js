98caefadd2128d4545e19efa580717a4b1c87c02
voroshilova <voroshilova.av@yandex.ru>
2022-10-16 10:37:17 +0400

    feat: add coordinate detection and permission to request geo-positioning

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert } from 'react-native';
+import * as Location from 'expo-location';
+import Loading from './Loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = '73da943d35f5f03ad9e59ba316761a1b';
+export default class extends React.Component {
+  
+  state = {
+    isLoading: true
+  }
+  
+  getLocation = async() => {    
+
+    try {
+      await Location.requestForegroundPermissionsAsync();      
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false});
+      //сделать запрос к API
+    } catch(error){
+      Alert.alert('Не могу определить местоположение', "Очень грустно:(")
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const {isLoading} = this.state;
+    return (
+      isLoading ? <Loading /> : null      
+    );
+  } 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
